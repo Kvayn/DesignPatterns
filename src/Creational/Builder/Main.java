264adc843053e921d017d942e991cd05dbe38d0b
Serhii Dorodko <sdorodko@gmail.com>
2020-10-26 17:19:14 +0200

    Add builder implementation

--- src/Creational/Builder/Main.java
+++ src/Creational/Builder/Main.java
@@ -2,6 +2,12 @@ package Creational.Builder;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Builder main output");
+        User serhii = new User.UserBuilder("Serhii", "Dorodko")
+                .age(27)
+                .phone("0678888888")
+                .address("Home street 1234")
+                .build();
+
+        System.out.println(serhii);
     }
 }
